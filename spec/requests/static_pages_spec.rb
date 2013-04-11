02227c82d139c349baef024446d728b70ff05fc0
Kevin Spector <kspector7@gmail.com>
2013-04-11 16:42:40 -0400

    Functional Header

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	before { visit root_path }
-
 	subject { page }
 
-	it { should have_selector('title', text: 'MLM Portal') }
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_selector('title', text: 'MLM Portal') }
+		it { should have_selector('h1', text: 'Missing Link Music Client Portal')}
+		it { should have_link('logo', href: root_path, src: '/assets/mlmlogo.png' )}
+		
+	end
 end
