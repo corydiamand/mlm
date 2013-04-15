2dff29f735bd783d023365405d3a921922330138
Kevin Spector <kspector7@gmail.com>
2013-04-15 12:46:53 -0400

    Add session tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.authenticate(params[:session][:email],
+  							 params[:session][:password])
+
+  	if user.nil?
+  		#Create error
+  	else
+  		#Sign in
+  	end
+  end
 end
