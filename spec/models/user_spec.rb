5a9880629c39c03291921a3d9e95f30411d1bf6f
Kevin Spector <kspector7@gmail.com>
2013-04-16 11:41:46 -0400

    Working email method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,51 +24,52 @@ describe 'Users' do
 		end
 	end
 
-	describe "password encryption" do
+	describe "after being saved to the db" do
 		before { @user.save! }
 		after { @user.destroy }
 
-		it "should set the encrypted password" do
-			@user.encrypted_password.should_not be_blank
+		describe "password encryption" do
+
+			it "should set the encrypted password" do
+				@user.encrypted_password.should_not be_blank
+			end
+
+			describe "has_password? method" do
+
+				it "should be true if passwords match" do
+					@user.has_password?(@user.password).should be_true
+				end
+
+				it "should be false if the passwords don't match" do
+					@user.has_password?("invalid").should be_false
+				end
+			end
+
+			describe "authenticate method" do
+
+				it "should return nil on email/password mismatch" do
+					wrong_password_user = User.authenticate(@user.email, "wrongpass")
+					wrong_password_user.should be_nil
+				end
+
+				it "should return nil for an email with no user" do
+					nonexistent_user = User.authenticate("wrong@email.com", @user.password)
+					nonexistent_user.should be_nil
+				end
+
+				it "should return the user on email/password match" do
+					matching_user = User.authenticate(@user.email, @user.password)
+					matching_user.should == @user
+				end
+			end
 		end
 
-		describe "has_password? method" do
+		describe "find_by_email method" do
 
-			it "should be true if passwords match" do
-				@user.has_password?(@user.password).should be_true
+			it "should return the user" do
+				user = User.find_by_email("example@email.com")
+				user.should == @user
 			end
-
-			it "should be false if the passwords don't match" do
-				@user.has_password?("invalid").should be_false
-			end
-		end
-
-		describe "authenticate method" do
-
-			it "should return nil on email/password mismatch" do
-				wrong_password_user = User.authenticate(@user.email, "wrongpass")
-				wrong_password_user.should be_nil
-			end
-
-			it "should return nil for an email with no user" do
-				nonexistent_user = User.authenticate("wrong@email.com", @user.password)
-				nonexistent_user.should be_nil
-			end
-
-			it "should return the user on email/password match" do
-				matching_user = User.authenticate(@user.email, @user.password)
-				matching_user.should == @user
-			end
-		end
-	end
-
-	describe "find_by_email method" do
-		before { @user.save! }
-		after { @user.destroy }
-
-		it "should return the user" do
-			user = User.find_by_email("example@email.com")
-			user.should == @user
 		end
 	end
 end
