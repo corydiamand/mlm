cbdef20eb85f71e3d8e5fb2bda3dc8e2e3ecccaa
Kevin Spector <kspector7@gmail.com>
2013-04-23 17:53:05 -0400

    Failing fields

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -90,23 +90,22 @@ describe 'User Pages' do
 	    it { should have_content('error') }
 	  end
 
-	  # describe "with valid information" do
-	  #   let(:new_first_name)  { "New Name" }
-	  #   let(:new_email) { "new@example.com" }
-	  #   before do
-	  #    fill_in 'user_first_name',            with: new_first_name
-	  #    fill_in 'user_email',                 with: new_email
-	  #    fill_in 'user_password',              with: @user.password
-	  #    fill_in 'user_password_confirmation', with: @user.password
-	  #    click_button "Save changes"
-	  #   end
-          
-   #    it { should have_selector('h2', content: 'Update your account') }
+	  describe "with valid information" do
+	    let(:new_first_name)  { "New Name" }
+	    let(:new_email) { "new@example.com" }
+	    before do
+       visit edit_user_path(@user.id)
+	     fill_in "First name",            with: new_first_name
+	     fill_in "Email",                 with: new_email
+	     fill_in "Password",              with: @user.password
+	     fill_in "Password confirmation", with: @user.password
+	     click_button "Save changes"
+	    end
 
-	  #   it { should have_selector('div.alert.alert-success') }
-	  #   specify { @user.reload.first_name.should == new_first_name }
-   #    specify { @user.reload.email.should == new_email }
-   #  end
+	    it { should have_selector('div.alert.alert-success') }
+	    specify { @user.reload.first_name.should == new_first_name }
+      specify { @user.reload.email.should == new_email }
+    end
 	end
 end
 
