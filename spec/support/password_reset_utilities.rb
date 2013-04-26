78b151d15cf7319599af7f98f22842681279f88c
Kevin Spector <kspector7@gmail.com>
2013-04-26 11:07:56 -0400

    passing test suite

--- spec/support/password_reset_utilities.rb
+++ spec/support/password_reset_utilities.rb
@@ -1,8 +1,8 @@
 include ApplicationHelper
 
 def request_new_password(user)
-  visit root_path
-  click_button 'Forgot Password?'
+  visit new_password_reset_path
   fill_in 'Email',    with: @user.email
   click_button 'Reset Password'
+  user.reload
 end
