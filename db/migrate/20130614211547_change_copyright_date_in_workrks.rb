d8cd46f3c6ead808a9a695844df40cf1247aecc4
Kevin Spector <kspector7@gmail.com>
2013-06-19 16:28:41 -0400

    update migrations3

--- db/migrate/20130614211547_change_copyright_date_in_workrks.rb
+++ db/migrate/20130614211547_change_copyright_date_in_workrks.rb
@@ -1,6 +1,6 @@
 class ChangeCopyrightDateInWorkrks < ActiveRecord::Migration
   def up
-    change_column :works, :copyright_date, :copyright_date_temp
+    rename_column :works, :copyright_date, :copyright_date_temp
     add_column :works, :copyright_date, :date
     # Work.reset_column_information
     # Work.find.each { |w| w.update_attribute(:copyright_date, c.copyright_date_temp) }
