cf1fdbb871b9558fe0afad9c508f078d6410b045
Kevin Spector <kspector7@gmail.com>
2013-06-19 16:14:42 -0400

    update migrations

--- db/migrate/20130603184111_change_date_in_statements.rb
+++ db/migrate/20130603184111_change_date_in_statements.rb
@@ -1,9 +1,13 @@
 class ChangeDateInStatements < ActiveRecord::Migration
   def up
-    change_column :statements, :date, :date
-  end
+    rename_column :statements, :date, :date_temp
+    add_column :statements, :date, :date
 
+    Statement.reset_column_information
+    Statement.find.each { |s| s.update_attribute(:date, c.date_temp) }
+    remove_column :statements, :date_temp
+  end
+  
   def down
-    change_column :statements, :date, :string
   end
 end
--- db/migrate/20130614211547_change_copyright_date_in_workrks.rb
+++ db/migrate/20130614211547_change_copyright_date_in_workrks.rb
@@ -1,9 +1,13 @@
 class ChangeCopyrightDateInWorkrks < ActiveRecord::Migration
   def up
-    change_column :works, :copyright_date, :date
+    change_column :works, :copyright_date, :copyright_date_temp
+    add_column :works, :copyright_date, :date
+
+    Work.reset_column_information
+    Work.find.each { |w| w.update_attribute(:copyright_date, c.copyright_date_temp) }
+    remove_column :works, :copyright_date_temp
   end
 
   def down
-    change_column :works, :copyright_date, :string
   end
 end
