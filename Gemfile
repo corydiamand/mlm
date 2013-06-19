cd3d6dc27971c448b90f91a16d57affe89ff11a3
Kevin Spector <kspector7@gmail.com>
2013-06-19 15:54:26 -0400

    change production.rb

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ group :development do
 end
 # Gems used only for assets and not required
 # in production environments by default.
-group :assets do
+group :assets, :production do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'bootstrap-sass'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ FirstPass::Application.configure do
   config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
-  config.assets.manifest = YOUR_PATH
+  #config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
