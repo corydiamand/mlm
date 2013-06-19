5a5c4c429abcd878a4d5b78d6541040b5e1a53f7
Kevin Spector <kspector7@gmail.com>
2013-06-19 16:25:07 -0400

    update migrations2

--- db/migrate/20130603184111_change_date_in_statements.rb
+++ db/migrate/20130603184111_change_date_in_statements.rb
@@ -2,12 +2,11 @@ class ChangeDateInStatements < ActiveRecord::Migration
   def up
     rename_column :statements, :date, :date_temp
     add_column :statements, :date, :date
-
-    Statement.reset_column_information
-    Statement.find.each { |s| s.update_attribute(:date, c.date_temp) }
+    # Statement.reset_column_information
+    # Statement.find.each { |s| s.update_attribute(:date, c.date_temp) }
     remove_column :statements, :date_temp
   end
-  
+
   def down
   end
 end
