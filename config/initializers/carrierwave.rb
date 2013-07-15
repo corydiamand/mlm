f8acfedf9ca34cee699d870468ef58b7c1226ac2
Kevin Spector <kspector7@gmail.com>
2013-07-15 12:37:49 -0400

    user_uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV["S3_SECRET"]
     }
     config.fog_directory = ENV["S3_BUCKET"]
+    config.fog_directory = 'user_uploads'
     config.fog_public    = false
   end 
 end
