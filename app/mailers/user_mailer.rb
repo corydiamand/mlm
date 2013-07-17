d0b99877f1505bbfe462d6b273a9f6ded2462710
Kevin Spector <kspector7@gmail.com>
2013-07-17 14:36:06 -0400

    force ssl

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "password_resets@missinglinkmusic.com"
+  default from: "no-reply@missinglinkmusic.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
