a5f533228e6c9afbd88718d5eb36ce3519e15367
Cory Diamand <cory@postpourri.com>
2014-02-01 23:23:19 -0500

    add view all statements feature

--- app/controllers/admin/statements_controller.rb
+++ app/controllers/admin/statements_controller.rb
@@ -1,9 +1,13 @@
 class Admin::StatementsController < Admin::ApplicationController
-  before_filter :admin_current_user
+  before_filter :admin_current_user, only: [:index]
 
   def index
     @statements = @user.statements
     render "statements/index"
+  end
+
+  def indexall
+  	@statements = Statement.includes(:user).find(:all, :order => "date").paginate(page: params[:page],:per_page => 50)
   end
 
 end
