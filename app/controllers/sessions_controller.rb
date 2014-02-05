65dc39f8675403d242de527fd04f36da23ee9f5b
Cory Diamand <corydiamand@gmail.com>
2014-02-05 05:21:50 -0500

    remove session creation to troubleshoot bug in staging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in user
       current_user.admin? ? redirect_to(admin_users_path) : redirect_to(user_statements_path(user))
       flash[:success] = 'Successfully signed in!'
-      Session.create(:login => Time.now, :user_id => current_user.id)
+      #Session.create(:login => Time.now, :user_id => current_user.id)
   	else
   		flash.now[:error] = "Invalid email/password combination"
       render 'static_pages/home'
@@ -19,8 +19,8 @@ class SessionsController < ApplicationController
   def destroy
     
     #puts Session.find_by_user_id(current_user.id).class
-    sesh = Session.where(:user_id => current_user.id).order("created_at DESC").first.id
-    Session.find(sesh).update_attribute("logout", Time.now)
+    #sesh = Session.where(:user_id => current_user.id).order("created_at DESC").first.id
+    #Session.find(sesh).update_attribute("logout", Time.now)
     sign_out
     redirect_to root_path
   end
