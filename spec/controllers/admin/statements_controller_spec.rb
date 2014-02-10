4a75e71c883b927f9788a9fbdb396d08ee53fa3a
Cory Diamand <cory@postpourri.com>
2014-02-10 17:19:38 -0500

    controller tests for destroy in users and statements controllers

--- spec/controllers/admin/statements_controller_spec.rb
+++ spec/controllers/admin/statements_controller_spec.rb
@@ -1,33 +1,24 @@
 require 'spec_helper'
 
 describe Admin::StatementsController do
-	let!(:admin) { FactoryGirl.create(:admin) }
-
-
-	describe "Delete destroy" do
-		#@statement = FactoryGirl.create(:statement)
+	Admin::StatementsController.skip_filter :signed_in_user, :admin_user, :pending_count
 
 	before :each do 
-		FactoryGirl.create(:admin)
+		@request.env['HTTP_REFERER'] = 'http://localhost:3000/admin_statements'
 		@statement = FactoryGirl.create(:statement)
 	end
 
+	describe "Delete destroy" do
+
 		it "lowers the number of statements by 1" do
 			first_count = Statement.count
-			puts Statement.all.inspect
-			puts @statement.inspect
 			delete :destroy, id: 1
-			delete "destroy", :id => 1
-			puts response.body
-			#@statement.destroy
 			second_count = Statement.count
 			first_count.should eq(1)
-			second_count.should eq 0
+			second_count.should eq(0)
 		end
 
 		it "redirects to all statement page" do 
-			#delete :destroy, id: @statement 
-			#response.should redirect_to admin_statements_url 
 		end 
 	end 
 end
---
+++ spec/controllers/admin/user_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Admin::UsersController do
+	Admin::UsersController.skip_filter :signed_in_user, :admin_user, :pending_count
+
+	before :each do 
+		@request.env['HTTP_REFERER'] = 'http://localhost:3000/admin_users'
+		@user = FactoryGirl.create(:user)
+	end
+
+	describe "Delete destroy" do
+
+		it "lowers the number of users by 1" do
+			first_count = User.count
+			delete :destroy, id: @user.id
+			second_count = User.count
+			first_count.should eq(1)
+			second_count.should eq(0)
+		end
+
+		it "redirects to all user page" do 
+		end 
+	end 
+end
