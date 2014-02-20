b09bb5bb278d1cb919e9ce525326c90530897f2e
Cory Diamand <cory@postpourri.com>
2014-02-20 02:56:23 -0500

    this migration is bad at the moment

--- db/migrate/20140219230516_remove_user_id_from_statements.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromStatements < ActiveRecord::Migration
-  def up
-    remove_column :statements, :user_id
-  end
-
-  def down
-    add_column :statements, :user_id, :integer
-  end
-end
